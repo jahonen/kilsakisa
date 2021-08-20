20b4884765d400f18aa4137c36e5731354025c82
Ahonen <jukkisahonen@gmail.com>
2021-08-20 11:17:27 +0300

    changed auth to cognito user pools

--- src/App.js
+++ src/App.js
@@ -79,15 +79,11 @@ function App() {
       <div style={{marginBottom: 30}}>
         {
           notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
+            <div key={note.id}>
+              <h2>{note.description}</h2>
               <p>{note.distance}</p>
               <p>{note.teamsize}</p>
               <button onClick={() => deleteNote(note)}>Delete note</button>
-              {
-                note.image && <img src={note.image} style={{width: 400}} />
-              }
             </div>
           ))
         }
